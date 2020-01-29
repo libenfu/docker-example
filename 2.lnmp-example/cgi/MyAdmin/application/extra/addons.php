4e4a215790ae7fa4616b3bdd0220cf31debf31a7
len <libenfu@bitz.email>
2020-01-29 17:14:06 +0800

    fix

--- 2.lnmp-example/cgi/MyAdmin/application/extra/addons.php
+++ 2.lnmp-example/cgi/MyAdmin/application/extra/addons.php
@@ -4,8 +4,37 @@ return array (
   'autoload' => false,
   'hooks' => 
   array (
+    'ems_send' => 
+    array (
+      0 => 'faems',
+    ),
+    'ems_notice' => 
+    array (
+      0 => 'faems',
+    ),
+    'app_init' => 
+    array (
+      0 => 'log',
+    ),
+    'testhook' => 
+    array (
+      0 => 'markdown',
+    ),
+    'upload_after' => 
+    array (
+      0 => 'thumb',
+    ),
   ),
   'route' => 
   array (
+    '/example$' => 'example/index/index',
+    '/example/d/[:name]' => 'example/demo/index',
+    '/example/d1/[:name]' => 'example/demo/demo1',
+    '/example/d2/[:name]' => 'example/demo/demo2',
+    '/third$' => 'third/index/index',
+    '/third/connect/[:platform]' => 'third/index/connect',
+    '/third/callback/[:platform]' => 'third/index/callback',
+    '/third/bind/[:platform]' => 'third/index/bind',
+    '/third/unbind/[:platform]' => 'third/index/unbind',
   ),
 );
